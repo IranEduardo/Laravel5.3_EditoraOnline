d0b100a2610eabfe31ae0d367212def9440da30a
Iran Eduardo <iranedu@hotmail.com>
2017-08-24 01:51:45 -0300

    1) Alteração no model de Category para que o mesmo implemente a interface TableInterface (biblioteca Bootstrapper), essa interface possui dois metodos que vc deve implementar para poder ter o cabeçaho e o valor dos atributos do model em uma table. 2) Alteração da view de Categories (index) para usar a Table da biblioteca do Boostrapper

--- app/Category.php
+++ app/Category.php
@@ -2,11 +2,30 @@
 
 namespace App;
 
+use Bootstrapper\Interfaces\TableInterface;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Category extends Model implements TableInterface
 {
     protected $fillable = [
         'name'
     ];
+
+
+    public function getTableHeaders()
+    {
+        // TODO: Implement getTableHeaders() method.
+        return ['Id', 'Nome'];
+    }
+
+    public function getValueForHeader($header)
+    {
+        // TODO: Implement getValueForHeader() method.
+        switch($header) {
+            case 'Id':
+                return $this->id;
+            case 'Nome':
+                return $this->name;
+        }
+    }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -8,35 +8,31 @@
         </div>
 
         <div class="row">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($categories as $category)
-                    <tr>
-                        <td>{{ $category->id }}</td>
-                        <td>{{ $category->name }}</td>
-                        <td>
-                            <ul class="list-inline">
-                                <li><a href="{{route('categories.edit', ['category' => $category->id]) }}">Editar</a></li>
-                                <li>|</li>
-                                <li>
-                                    <a href="{{route('categories.destroy', ['category' => $category->id]) }}"
-                                        onclick="document.getElementById('delete-form-{{$category->id}}').submit(); return false">Excluir</a>
-                                    {!! Form::open(['route' => ['categories.destroy','category' => $category->id],
-                                                    'method' => 'DELETE', 'id' => "delete-form-$category->id", 'class' => 'form', 'style' => 'display:none']) !!}
-                                    {!! Form::close() !!}
-                                </li>
-                            </ul>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
+
+            {!!
+               Table::withContents($categories->items())->striped()
+                    ->callback('Ações', function($field, $category) {
+
+                           $form =  Form::open(['route' => ['categories.destroy','category' => $category->id],
+                                                'method' => 'DELETE', 'id' => "delete-form-{$category->id}", 'class' => 'form', 'style' => 'display:none']);
+                           $LinkEdit    = Button::link('Editar')->asLinkTo(route('categories.edit', ['category' => $category->id]));
+                           $LinkDestroy = Button::link('Excluir')->asLinkTo(route('categories.destroy', ['category' => $category->id]))
+                                           ->addAttributes([
+                                                     'onclick' => "document.getElementById(\"delete-form-$category->id\").submit(); return false"
+                                            ]);
+                           $formClose =  Form::close();
+
+                           return
+                                "<ul class=\"list-inline\">" .
+                                      "<li>" . $LinkEdit . "</li>" .
+                                      "<li>|</li>" .
+                                      "<li>" . $LinkDestroy . $form . $formClose .
+                                       "</li>" .
+                                "</ul>";
+
+                   })
+             !!}
+
             {{ $categories->links() }}
         </div>
     </div>
